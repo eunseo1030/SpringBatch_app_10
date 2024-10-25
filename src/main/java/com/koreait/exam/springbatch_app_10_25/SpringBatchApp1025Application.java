02bbeb695c850c20ea787c88c61a61e05dff2eee
eunseo1030 <eunseo20001030@gmail.com>
2024-10-25 09:37:30 +0900

    프로젝트 세팅

---
+++ src/main/java/com/koreait/exam/springbatch_app_10_25/SpringBatchApp1025Application.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.springbatch_app_10_25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchApp1025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchApp1025Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/koreait/exam/springbatch_app_10_25/SpringBatchApp1025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.springbatch_app_10_25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApp1025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
